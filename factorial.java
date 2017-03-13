ec59bfc65012d21e9f49ac537b9fd759d7b7c38f
adri913c <adri.91.3c@gmail.com>
2017-03-13 14:21:05 +0100

    refactorizacion

--- factorial.java
+++ factorial.java
@@ -7,7 +7,7 @@ public class factorial {
 		Scanner scanInput = new Scanner(System.in);
 		System.out.print("Introduzca un numero: ");
 		long num = scanInput.nextLong();
-		long fac = 1;//prueba
+		long fac = 1;
 		for (long i=1; i<=num; ++i) {
 			fac = fac * i;
 		}
