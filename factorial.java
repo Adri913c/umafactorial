15046f76d1aa93fc74f3249112cbabe2ed7e5ae8
adri913c <adri.91.3c@gmail.com>
2017-03-13 14:18:56 +0100

    implementacion inicial

--- factorial.java
+++ factorial.java
@@ -2,12 +2,12 @@
 import java.util.Scanner;
 
 public class factorial {
-	
+
 	public static void main(String []args) {
 		Scanner scanInput = new Scanner(System.in);
 		System.out.print("Introduzca un numero: ");
 		long num = scanInput.nextLong();
-		long fac = 1;
+		long fac = 1;//prueba
 		for (long i=1; i<=num; ++i) {
 			fac = fac * i;
 		}
